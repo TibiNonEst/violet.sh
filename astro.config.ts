62962bc9347ea40eaf3b1c0ce96cf60c82633e01
tibs <tibs@tibinonest.me>
2024-11-21 00:13:08 -0500

    Improve accessibility

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+    build: {
+        inlineStylesheets: "always"
+    },
     image: {
         domains: [ "static.tibs.gay" ]
     }
